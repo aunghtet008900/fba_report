eaae54b8624bee4203befb595fd1f716a66f624c
Joe <webmaster@bookculture.com>
2013-04-12 20:07:39 -0400

    moved config to a required module

---
+++ config.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+module MyConfig
+  CONFIG = {
+            :adapter => 'sqlite3',
+            :database => File.expand_path('../db/production.sqlite3', __FILE__),
+            :pool => 5,
+            :timeout => 5000
+  }
+end
--- show-index.rb
+++ show-index.rb
@@ -2,15 +2,10 @@
 
 require 'sqlite3'
 require 'active_record'
-require 'yaml'
-
+require_relative 'config.rb'
 require_relative 'lib/gem_example'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => File.expand_path('../db/production.sqlite3', __FILE__)
-)
-
+ActiveRecord::Base.establish_connection(MyConfig::CONFIG)
 
 # this class definition should be moved into lib/gem_example/employee.rb
 class Employee < ActiveRecord::Base
