fb3811c15a7346d6ac66fea29883242e850a784f
Joe <webmaster@bookculture.com>
2013-04-15 17:52:20 -0400

    Add dbinfo.rb to give a quick overview of what's in the db

---
+++ dbinfo.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'sqlite3'
+require 'active_record'
+require_relative 'db/config'
+require_relative 'lib/book_culture_lib'
+
+ActiveRecord::Base.establish_connection(MyConfig::CONFIG)
+
+## Show all orders:
+#BookCultureLib::AmazonOrder.all.each do |ord|
+#  puts ord
+#end
+
+puts
+
+puts "The attributes:"
+BookCultureLib::AmazonOrder.attribute_names.each do |atr|
+  puts "  " + atr.to_s
+end
+
+puts "Number of database records:"
+puts "  " + BookCultureLib::AmazonOrder.count.to_s
+puts "first purchase date:"
+puts "  " + BookCultureLib::AmazonOrder.first.purchase_date.to_s
+puts "last purchase date:"
+puts "  " + BookCultureLib::AmazonOrder.last.purchase_date.to_s
+
+puts
